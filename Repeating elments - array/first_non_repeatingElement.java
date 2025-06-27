9ae5e59e82aee54ce169a9f0897c48bd732b43da
Maha561 <mahalakshmianandan04@gmail.com>
2025-06-27 16:50:12 +0530

    added

---
+++ Repeating elments - array/first_non_repeatingElement.java
@@ -0,0 +1,27 @@
+
+//Find the first non-repeating element in a given array arr of integers and if there is not present any non-repeating element then return 0
+
+//Note: The array consists of only positive and negative integers and not zero.//
+
+class Solution {
+    public int firstNonRepeating(int[] arr) {
+        HashMap<Integer,Integer> count = new HashMap<>();
+        for(int num : arr){
+            
+            count.put(num ,count.getOrDefault(num,0)+1);
+        }
+        
+        for(int i = 0 ; i<arr.length;i++){
+            
+            if(count.get(arr[i])==1){
+                
+                // return the element
+                return arr[i];
+            }
+        }
+        return 0;
+        
+    }
+}
+
+// gfg link : https://www.geeksforgeeks.org/problems/non-repeating-element3958/1
--- Repeating elments - array/first_repeating_element.java
+++ Repeating elments - array/first_repeating_element.java
@@ -1,5 +1,28 @@
-package Repeating elments - array;
+// Given an array arr[], find the first repeating element. The element should occur more than once and the index of its first occurrence should be the smallest.
 
-public class first_repeating_element {
-    
+class Solution {
+    public static int firstRepeated(int[] arr) {
+         HashMap<Integer,Integer> count = new HashMap<>();
+         // getting frequency
+         for(int num : arr){
+            count.put(num,count.getOrDefault(num,0)+1);
+         }
+         
+         // find the first repeating
+         for(int i =0; i<arr.length;i++){
+             // get method ,get the frequency
+             if(count.get(arr[i]) >1){
+                 
+                 return i+1;
+             }
+         }
+         return -1;
+         
+         
+         
+         
+        
+    }
 }
+
+// gfg link : https://www.geeksforgeeks.org/problems/first-repeating-element4018/1
