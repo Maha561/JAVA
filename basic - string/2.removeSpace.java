df00558b9078c3655c9b1d5d78578cd1ee081b8f
Maha561 <mahalakshmianandan04@gmail.com>
2025-07-03 16:47:25 +0530

    added

--- basic - string/2.removeSpace.java
+++ basic - string/2.removeSpace.java
@@ -1,3 +1,14 @@
-public class 2.removeSpace {
-    
+
+
+// question : Given a string s. Your task is to remove spaces from it. 
+
+class Solution {
+
+    String modify(String s) {
+        String result = s.replace(" ","");
+        return result;
+        
+    }
 }
+
+// https://www.geeksforgeeks.org/problems/remove-spaces0128/1?page=1&category=Strings&difficulty=Basic&sortBy=submissions
