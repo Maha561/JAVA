d46b9dbe13df33088416492d0ddd1cf65d37a4fc
Maha561 <mahalakshmianandan04@gmail.com>
2025-07-03 17:24:19 +0530

    added

--- basic - string/easy-string/3.binaryString.java
+++ basic - string/easy-string/3.binaryString.java
@@ -1,5 +1,19 @@
-package easy-string;
 
-public class 3.binaryString {
-    
+//  Given a non-empty sequence of characters s, return true if sequence is Binary, else return false.
+
+class Solution {
+    boolean isBinary(String s) {
+        for(int i =0;i<s.length();i++){
+            if(s.charAt(i)!='0'&&s.charAt(i)!='1'){
+                
+                return false;
+
+            }
+        }
+        return true;
+        
+    }
 }
+
+//https://www.geeksforgeeks.org/problems/check-for-binary/1?page=1&category=Strings&difficulty=Basic&sortBy=submissions
+
