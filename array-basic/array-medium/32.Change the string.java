080493d499a97cde482f7fee81fd6887359481a8
Maha561 <mahalakshmianandan04@gmail.com>
2025-09-04 19:13:16 +0530

    added

---
+++ array-basic/array-medium/32.Change the string.java
@@ -0,0 +1,21 @@
+
+
+// User function Template for Java
+
+class Solution {
+    String modify(String s) {
+        
+        
+        //StringBuilder result = new StringBuilder();
+        
+       if(Character.isLowerCase(s.charAt(0))){
+           
+           return  s.toLowerCase();
+       }
+       else{
+           return s.toUpperCase();
+       }
+    }
+}
+
+// https://www.geeksforgeeks.org/problems/change-the-string3541/1
